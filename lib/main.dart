8e1cb8a2f64115b6c5ddaf82a2c8a60f14898657
Yushan Fernando <yushanwebdev@gmail.com>
2024-06-23 16:10:39 +0530

    feat: Stateful/Stateless Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/pages/first_page.dart';
-import 'package:flutter_demo/pages/second_page.dart';
+import 'package:flutter_demo/pages/counter_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,18 +8,8 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  static List names = ["John", "Doe", "Jane", "Doe"];
-
-  void userTapped() {
-    print("User tapped the screen!");
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-      routes: {'/secondpage': (context) => SecondPage()},
-    );
+    return MaterialApp(home: CounterPage());
   }
 }

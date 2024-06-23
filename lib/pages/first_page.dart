cc492ade6582d1c0a5802aab613e182fff23d3ff
Yushan Fernando <yushanwebdev@gmail.com>
2024-06-23 14:57:58 +0530

    feat: Bottom Navigation Bar

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,44 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/pages/home_page.dart';
+import 'package:flutter_demo/pages/profile_page.dart';
 import 'package:flutter_demo/pages/second_page.dart';
+import 'package:flutter_demo/pages/settings_page.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
+  FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  void _navigationBottomBar(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  int _currentIndex = 0;
+
+  List _pages = [
+    HomePage(),
+    ProfilePage(),
+    SettingsPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('First Page'),
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-                child: Icon(
-              Icons.account_circle,
-              size: 48,
-            )),
-            ListTile(
-                leading: Icon(Icons.home),
-                title: Text('Home'),
-                onTap: () => {
-                      Navigator.pop(context),
-                      Navigator.pushNamed(context, "/secondpage"),
-                    }),
-            ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-                onTap: () => {
-                      Navigator.pushNamed(context, "/secondpage"),
-                    }),
-          ],
-        ),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/secondpage');
-          },
-          child: Text('Go to Second Page'),
-        ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _navigationBottomBar,
+        items: [
+          // home
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+
+          // profile
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+
+          // settings
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

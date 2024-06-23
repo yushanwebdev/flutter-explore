a5bfa5b63f5b5121e44042585b4080deddf99d4e
Yushan Fernando <yushanwebdev@gmail.com>
2024-06-23 13:57:15 +0530

    feat: Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,26 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  static List names = ["John", "Doe", "Jane", "Doe"];
+
+  void userTapped() {
+    print("User tapped the screen!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          backgroundColor: Colors.blue[100],
-          body: Center(
-            child: Container(
-                width: 300,
-                height: 300,
-                color: Colors.deepPurple,
-                child: Text("Mitch KOKO")),
-          ),
-        ));
+            backgroundColor: Colors.blue[100],
+            body: GestureDetector(
+              onTap: userTapped,
+              child: Container(
+                width: 400,
+                height: 400,
+                color: Colors.blue[500],
+                child: Center(child: Text("Hello World!")),
+              ),
+            )));
   }
 }
